0d7d2d3c85e6c6be9b83813408f9497226756681
shudson <shudson@anl.gov>
2017-07-24 13:05:45 -0500

    Fix unit tests so passes

--- code/unit_tests/test_manager_main.py
+++ code/unit_tests/test_manager_main.py
@@ -11,61 +11,62 @@ def make_criteria_and_specs_0():
     sim_specs={'f': [np.linalg.norm], 'in':['x'], 'out':[('g','float')], 'params':{}}
     gen_specs={'f': [np.random.uniform], 'in':[], 'out':[('x','float'),('priority','float')], 'params':{}}
     exit_criteria={'sim_eval_max':10}
+    H0=[]
 
-    return sim_specs, gen_specs, exit_criteria
+    return sim_specs, gen_specs, exit_criteria, H0
 
 def make_criteria_and_specs_1():
     sim_specs={'f': [np.linalg.norm], 'in':['x'], 'out':[('g','float')], 'params':{}}
     gen_specs={'f': [np.random.uniform], 'in':[], 'out':[('x','float'),('priority','float')], 'params':{}}
     exit_criteria={'sim_eval_max':10, 'stop_val':('g',-1), 'elapsed_clock_time':0.5}
+    H0=[]    
 
-    return sim_specs, gen_specs, exit_criteria
+    return sim_specs, gen_specs, exit_criteria, H0
 
 
 def test_termination_test():
     # termination_test should be False when we want to stop
-
-    sim_specs_0, gen_specs_0, exit_criteria_0 = make_criteria_and_specs_0()
-    H, H_ind = man.initiate_H(sim_specs_0, gen_specs_0, exit_criteria_0['sim_eval_max']) 
-    assert(man.termination_test(H, H_ind, exit_criteria_0))
+    sim_specs_0, gen_specs_0, exit_criteria_0, H0_0 = make_criteria_and_specs_0()
+    H, H_ind = man.initialize_H(sim_specs_0, gen_specs_0, exit_criteria_0['sim_eval_max'],H0=H0_0)
+    assert(man.termination_test(H, H_ind, exit_criteria_0, len(H0_0)))
 
 
 
     # Shouldn't terminate
-    sim_specs, gen_specs, exit_criteria = make_criteria_and_specs_1()
-    H, H_ind = man.initiate_H(sim_specs, gen_specs, exit_criteria['sim_eval_max']) 
-    assert(man.termination_test(H, H_ind, exit_criteria))
+    sim_specs, gen_specs, exit_criteria, H0 = make_criteria_and_specs_1()
+    H, H_ind = man.initialize_H(sim_specs, gen_specs, exit_criteria['sim_eval_max'],H0=H0) 
+    assert(man.termination_test(H, H_ind, exit_criteria, len(H0)))
     # 
 
 
     # Terminate because we've found a good 'g' value
-    H, H_ind = man.initiate_H(sim_specs, gen_specs, exit_criteria['sim_eval_max']) 
+    H, H_ind = man.initialize_H(sim_specs, gen_specs, exit_criteria['sim_eval_max'],H0=H0) 
     H['g'][0] = -1
     H_ind = 1
-    assert(not man.termination_test(H, H_ind, exit_criteria))
+    assert(not man.termination_test(H, H_ind, exit_criteria, len(H0)))
     # 
 
     
     # Terminate because everything has been given.
-    H, H_ind = man.initiate_H(sim_specs, gen_specs, exit_criteria['sim_eval_max']) 
+    H, H_ind = man.initialize_H(sim_specs, gen_specs, exit_criteria['sim_eval_max'],H0=H0) 
     H['given'] = np.ones
-    assert(not man.termination_test(H, H_ind, exit_criteria))
+    assert(not man.termination_test(H, H_ind, exit_criteria, len(H0)))
     # 
     
 
     # Terminate because enough time has passed
-    H, H_ind = man.initiate_H(sim_specs, gen_specs, exit_criteria['sim_eval_max']) 
+    H, H_ind = man.initialize_H(sim_specs, gen_specs, exit_criteria['sim_eval_max'],H0=H0) 
     H_ind = 4
     H['given_time'][0] = time.time()
     time.sleep(0.5)
-    assert(not man.termination_test(H, H_ind, exit_criteria))
+    assert(not man.termination_test(H, H_ind, exit_criteria, len(H0)))
     # 
 
 
 def test_decide_work_and_resources():
 
-    sim_specs, gen_specs, exit_criteria = make_criteria_and_specs_1()
-    H, H_ind = man.initiate_H(sim_specs, gen_specs, exit_criteria['sim_eval_max']) 
+    sim_specs, gen_specs, exit_criteria, H0 = make_criteria_and_specs_1()
+    H, H_ind = man.initialize_H(sim_specs, gen_specs, exit_criteria['sim_eval_max'],H0=H0) 
 
 
     # Don't give out work when idle is empty
@@ -79,8 +80,8 @@ def test_decide_work_and_resources():
     # Don't give more gen work than space in the history. 
     active_w = set()
     idle_w = set([1,2,3,4])
-    H_ind = len(H)-2 # Only want one more Gen Point
-    H['sim_id'][:len(H)-2] = np.arange(len(H)-2)
+    H_ind = len(H)-1 # Only want one more Gen Point
+    H['sim_id'][:len(H)-1] = np.arange(len(H)-1)
     Work = man.decide_work_and_resources(active_w, idle_w, H, H_ind, sim_specs, gen_specs)
     assert( len(Work) == 2) # Length 2 because first work element is all points to be evaluated, second element is gen point
     # 
@@ -89,8 +90,8 @@ def test_decide_work_and_resources():
 def test_update_history_x_in():
 
     # Don't take more points than there is space in history.
-    sim_specs, gen_specs, exit_criteria = make_criteria_and_specs_1()
-    H, H_ind = man.initiate_H(sim_specs, gen_specs, exit_criteria['sim_eval_max']) 
+    sim_specs, gen_specs, exit_criteria, H0 = make_criteria_and_specs_1()
+    H, H_ind = man.initialize_H(sim_specs, gen_specs, exit_criteria['sim_eval_max'],H0=H0) 
 
     O = np.zeros(2*len(H), dtype=gen_specs['out'])
     print(len(O))
