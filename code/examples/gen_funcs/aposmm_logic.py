ddedc3bfd3c86439b1747e4b1d741e8549daaaff
shudson <shudson@anl.gov>
2017-11-29 18:00:22 -0600

    fix docs

--- code/examples/gen_funcs/aposmm_logic.py
+++ code/examples/gen_funcs/aposmm_logic.py
@@ -2,18 +2,9 @@ from __future__ import division
 from __future__ import absolute_import
 
 import sys, os
-import numpy as np
-import scipy as sp
-from scipy import spatial
-from mpi4py import MPI
-
-from numpy.lib.recfunctions import merge_arrays
-
-from math import log, gamma, pi, sqrt
-
-from petsc4py import PETSc
-import nlopt
 
 def aposmm_aaatest():
+    
     """A simple test."""
+    
     pass
